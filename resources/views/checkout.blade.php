84edd67a661527b2b190363f1f0afbe257fe982e
Jacco Gritter <jacco.gritter@gmail.com>
2019-07-11 11:49:45 +0200

    user must add delivery address before payment

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -2,7 +2,27 @@
 
 @section('leftcontent')
 
-Uw gegevens
+<h5>Verzendgegevens:</h5>
+<form action="/search" method="get">
+    <div class="form-group">
+      <input type="text" class="form-control" id="naam" name="naam" value="{{Auth::user()->name}}" readonly>
+    </div>
+    <div class="form-group">
+      <input  type="text" class="form-control zip" id="straatnaam" name="straatnaam" placeholder="Straatnaam">
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" id="huisnummer" name="huisnummer" placeholder="Huisnummer" ">
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" id="postcode" name="postcode" placeholder="Postcode">
+    </div>
+    <div class="form-group">
+      <input  type="text" class="form-control" id="woonplaats" name="woonplaats" placeholder="Woonplaats" onblur="displayPayBtn()">
+    </div>
+    
+</form>
+
+    
 
 @endsection
 
@@ -16,13 +36,22 @@ Uw gegevens
             <table class="table table-sm table-dark">
             <th>Merk</th><th>Naam</th><th>Prijs</th><th>Aantal</th>
             @foreach($wines as $wine)
-            <tr><td>{{$wine->brand}}</td><td>{{$wine->name}}</td><td>€{{ number_format($wine->pivot->myprice, 2) }}</td><td>{{$wine->pivot->quantity}}</td><td><a href="{{route('addone', $wine->orders->first()->id)}}"><i class="fas fa-plus fa-xs add"></i></a></td><td><a href="{{route('decreaseone', $wine->orders->first()->id)}}"><i class="fas fa-minus fa-xs add"></i></a></td></tr>
+            <tr><td>{{$wine->brand}}</td><td>{{$wine->name}}</td><td>€{{ number_format($wine->pivot->myprice, 2) }}</td><td>{{$wine->pivot->quantity}}</td><td></td></tr>
             @endforeach
             <tr><td><td><td><td></td></td></td></td></tr>
             <tr><td></td><td></td><td><b>€{{ number_format(Auth::user()->getOrderValue(),2)}}</b></td><td><b>{{Auth::user()->getCartQuantity()}}</b></td></tr>
             </table>
         </div>
 
-        <a href="{{ route('pay') }}" class="btn btn-primary" role="button">Betalen</a>
-        
+        <a href="{{ route('pay') }}" class="btn btn-primary" role="button" id="paybutton" style="display:none">Betalen</a>
+       
+<script>
+    function displayPayBtn() {
+        if( $("#straatnaam").val() != "" && $("#huisnummer").val() != "" && $("#postcode").val() != "" && $("#woonplaats").val() != "" )
+        {
+            $("#paybutton").show();
+        }
+    }
+       
+</script>
 @endsection
--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -10,6 +10,7 @@
      <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Vinum</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
